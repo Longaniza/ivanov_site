7f82617b09d7de6a3468fff6cb4e2adefdde8c85
Ivanov <ramireztiradojose@gmail.com>
2020-09-08 16:23:31 -0700

    Resume header added

--- src/Components/ResumeScreen.js
+++ src/Components/ResumeScreen.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
+import '../styles.css'
 
 export const ResumeScreen = () => {
     return (
         <div>
-            <h1>ResumeScreen</h1>
+            <div className="post-bar"></div>
+            <div className="header">
+                <img className="resume-image" src= "/ivanov.jpg"></img>
+                <h1>I'm Jose Ivanov Ramirez Tirado</h1>
+                <h5>Front End Developer</h5>
+                <div className="icons">
+                    <a href="https://www.linkedin.com/in/jose-ivanov-ramirez-tirado-b3200a178/"><img className="icon" src="/linkedin.png"></img></a>
+                    <a href="https://github.com/Longaniza"><img className="icon" src="/github.png"></img></a>
+                </div>
+                <a id="cv-button" className="btn btn-danger boton">Download CV</a>
+            </div>
         </div>
     )
 }
--- src/ui/NavBar.js
+++ src/ui/NavBar.js
@@ -1,18 +1,20 @@
 import React from 'react'
 import { Link, NavLink } from 'react-router-dom'
+import '../styles.css'
+
 
 export const NavBar = () => {
     return (
         <>
-        <nav class="navbar navbar-expand-lg navbar-light bg-primary">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-danger barra">
             <Link 
                 className="navbar-brand"
                 to="/"
             >
                 <img src="/mexicano.png" width="30" height="30" className="d-inline-block align-top" alt="" loading="lazy"></img>
             </Link>
-            <div class="navbar-collapse">
-                <div class="navbar-nav">
+            <div className="navbar-collapse">
+                <div className="navbar-nav">
                     <NavLink
                         activeClassName="active"
                         className="nav-item nav-link"
