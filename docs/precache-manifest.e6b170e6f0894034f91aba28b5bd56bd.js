dd08395a237dfaaea4d4a4d9087ec59023555a03
Ivanov <ramireztiradojose@gmail.com>
2020-09-12 12:20:30 -0700

    docs added

---
+++ docs/precache-manifest.e6b170e6f0894034f91aba28b5bd56bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3662ab8451240ed65f040ea4023ecf86",
+    "url": "/index.html"
+  },
+  {
+    "revision": "096d786e64c211a0d608",
+    "url": "/static/css/main.a21ffcff.chunk.css"
+  },
+  {
+    "revision": "19a261523f0754203e03",
+    "url": "/static/js/2.907fe0dc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.907fe0dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "096d786e64c211a0d608",
+    "url": "/static/js/main.4763b26f.chunk.js"
+  },
+  {
+    "revision": "40012a000469320341f7",
+    "url": "/static/js/runtime-main.4aa55700.js"
+  }
+]);
