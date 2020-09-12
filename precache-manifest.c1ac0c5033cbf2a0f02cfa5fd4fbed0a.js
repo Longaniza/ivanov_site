632a7360f6181ac209cbf1739b4832fa45516f6f
Ivanov <ramireztiradojose@gmail.com>
2020-09-12 13:27:22 -0700

    Updates

---
+++ precache-manifest.c1ac0c5033cbf2a0f02cfa5fd4fbed0a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b1e7b94b44bccba1a2aa3b4b119a98c",
+    "url": "/ivanov_site/index.html"
+  },
+  {
+    "revision": "087b09505338537aaa8b",
+    "url": "/ivanov_site/static/css/main.a21ffcff.chunk.css"
+  },
+  {
+    "revision": "cd205027cf3c81c7c0dc",
+    "url": "/ivanov_site/static/js/2.c8277e2c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ivanov_site/static/js/2.c8277e2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "087b09505338537aaa8b",
+    "url": "/ivanov_site/static/js/main.b2172290.chunk.js"
+  },
+  {
+    "revision": "3c4c1059d3f61f9a7dd9",
+    "url": "/ivanov_site/static/js/runtime-main.b0ddd9b6.js"
+  },
+  {
+    "revision": "9d35992f8f847e72232b3c168a477ca4",
+    "url": "/ivanov_site/static/media/ivanov.9d35992f.jpg"
+  }
+]);
